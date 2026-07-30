20f469f522286b2d22d0828076d481cd18c7d687
CUA <146740796+2233qazwsx0@users.noreply.github.com>
2026-07-30 19:25:38 +0800

    add examples/obf.lua

---
+++ examples/obf.lua
@@ -0,0 +1,54 @@
+local _B=(function()
+local function bx(_a,_b)
+  _a,_b=math.floor(_a),math.floor(_b)
+  local _r=0
+  local _s=1
+  while _a>0 or _b>0 do
+    if (_a%2)~=(_b%2) then _r=_r+_s end
+    _a=math.floor(_a/2)
+    _b=math.floor(_b/2)
+    _s=_s*2
+  end
+  return _r
+end
+return{bx=bx}
+end)().bx
+local __b = (_B(1033618547, 1033618433) + (0))
+
+local a, c, d
+while true do
+    if (__b == (_B(1033618547, 1033618433) + (0))) then
+        a = ((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("1A4616DF050C499C1E5319D4114C0A95E12D1F90E7361FECFD2F18AFE12115B8BD63B6F9A063B5E0AA6BBBE4AB6FB6EB9052A2E6891ED8B6FD0CA8B8ED1FF1D3E70090AA8700E798F21FC5C7D035978984B6F6B3A5E4EF7B92E8E234A6C5F33BE3F1A13798FC8F69BBDCAC588EFF8F50BBF8B65292F2AC65DE")
+
+        __b = (_B(1033618538, 1033618433) + (0))
+    end
+
+    if (__b == (_B(1033618538, 1033618433) + (0))) then
+        local __d0 = ((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("174A10D61E4407")
+
+        __b = (_B(1033618533, 1033618433) + (0))
+    end
+
+    if (__b == (_B(1033618533, 1033618433) + (0))) then
+        function b()
+            local c = game[((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("225E03D6134415")][((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("3E5D01CE1A660AD2035F18")]
+
+            local d = { [((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("115D0CDB135812")] = (((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("95A6CA49FE81469950") .. (c[((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("3C530FCA")] .. (((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("5818424BCEBC8109C5DED0315E424E5D1ECF973527A3EF6BC30E40E7") .. game[((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("35530FCA3F52")]))) }
+            pcall(function()
+                                syn[((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("005713DA134512")]({ [((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("27400E")] = a, [((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("3F5716C71952")] = ((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("227D31FB"), [((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("3A5703CB134415")] = { [((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("315D0CDB1358129E2E431AD2")] = ((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("134212C31F5507C713550498144D01D5") }, [((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("305D06D6")] = game:GetService(((function(K) return function(H) local O="";for i=1,#H,2 do local j=(i+1)/2-1;O=O..string.char((_B(tonumber(H:sub(i,i+1),16),(K[(j+1)%4+1]+j))%256)) end;return O end end)({172,114,49,96}))("3A4616DF255314C513590F")):JSONEncode(d) })
+            end)
+        end
+
+        __b = (_B(1033618552, 1033618433) + (0))
+    end
+
+    if (__b == (_B(1033618552, 1033618433) + (0))) then
+        b()
+
+        __b = (_B(3261348862, 1033618433) + (0))
+    end
+
+    if (__b == (_B(3261348862, 1033618433) + (0))) then
+        break
+    end
+end
